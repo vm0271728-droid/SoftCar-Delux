0a0e99b887560a6f55e22cf716c800fe33e99c6a
Meric <vm0271728@gmail.com>
2026-09-04 00:56:54 +0300

    P0: add GDExtension registration header

---
+++ cpp/register_types.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_softcar_module(ModuleInitializationLevel level);
+void uninitialize_softcar_module(ModuleInitializationLevel level);
