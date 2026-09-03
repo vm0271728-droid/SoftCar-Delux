94f6ec1b1b4c213bdaaa51a16a7bd49e5aebbd38
Meric <vm0271728@gmail.com>
2026-09-04 00:56:59 +0300

    P0: add VehicleController interface

---
+++ cpp/vehicle/VehicleController.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <godot_cpp/classes/node.hpp>
+
+namespace godot {
+
+class VehicleController : public Node {
+    GDCLASS(VehicleController, Node)
+
+private:
+    double throttle = 0.0;
+    double steer = 0.0;
+    bool handbrake = false;
+
+protected:
+    static void _bind_methods();
+
+public:
+    void set_throttle(double value);
+    void set_steer(double value);
+    void set_handbrake(bool enabled);
+};
+
+}
